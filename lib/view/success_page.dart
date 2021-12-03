339c93cdc062a05c31ddaeb97029b38dda357587
MURILO LOURENCO CABRAL <murilolcabral@gmail.com>
2021-12-03 18:22:59 -0300

    Add Page Success
    Adicionada página de sucesso

--- lib/view/success_page.dart
+++ lib/view/success_page.dart
@@ -9,12 +9,28 @@ class SuccessPage extends StatelessWidget {
       appBar: AppBar(
         title: Text('Sucesso'),
       ),
-      body: Center(
-        child: ElevatedButton(
-          onPressed: () {
-            Navigator.pop(context);
-            }, 
-          child: Text('Voltar para a página inicial'),),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.fromLTRB(0, 50, 0, 50),
+              child: Text('Que bom que chegou até aqui'),
+            ),
+            Center(
+              child: ElevatedButton(
+                onPressed: () {
+                  Navigator.pop(context);
+                  }, 
+                child: Padding(
+                  padding: const EdgeInsets.all(10),
+                  child: Text('Voltar para a página inicial',
+                  style: TextStyle(
+                    fontSize: 25
+                  ),),
+                ),),
+            ),
+          ],
+        ),
       ),
     );
   }
